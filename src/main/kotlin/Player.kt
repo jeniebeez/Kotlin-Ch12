d78fc66e38366ad72916529f50e128325766e7ec
jeniebeez <richardloyj@gmail.com>
2021-06-28 11:58:54 +0800

    c12-9

--- src/main/kotlin/Player.kt
+++ src/main/kotlin/Player.kt
@@ -1,6 +1,9 @@
 class Player {
     val name = "madrigal"
         get() = field.capitalize()
+        set(value) {
+            field = value.trim()
+        }
     fun castFireball(numFireballs:Int =2)=
         println("一杯Fireball酒應運而生。(x$numFireballs)")
 }
